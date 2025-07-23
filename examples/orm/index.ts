3da81382ded448de9687f6dbde5a5724dfd0bda6
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-07-23 11:55:35 +0200

    feat: random username in example

--- examples/orm/index.ts
+++ examples/orm/index.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 import { AbstractRepository, getRepository, initDB, runQuery } from '#orm'
+import { randomString } from '@kaynooo/utils'
 import { Database } from 'bun:sqlite'
 import { Tag } from './entities/tag'
 import { User } from './entities/user'
@@ -10,19 +11,16 @@ await initDB(new Database(path.resolve(import.meta.dir, 'data.sqlite')), [
   new AbstractRepository(Tag),
 ])
 
-runQuery('delete from user')
-runQuery('delete from tag')
-
 const tagRepo = getRepository(Tag)
-const tag = tagRepo.create({ name: 'My tag' })
+const tag = tagRepo.create({ name: `tag_${randomString(10)}` })
 
 const userRepo = getRepository(User)
-const newUser = userRepo.create({ username: 'My User', tag_id: tag.id })
+const newUser = userRepo.create({ username: `user_${randomString(10)}`, tag_id: tag.id })
 
 console.log('Created user:', newUser)
 console.log('User\'s tag:', newUser.getTag())
 console.log(userRepo.update(newUser.id, newUser))
 
-console.log('find in 3,4,5,6,7 9,10', userRepo.findAllBy({ where: { id: [3, 4, 5, 6, 7, 9, 10] } }))
+console.log('find in 3,4,5', userRepo.findAllBy({ where: { id: [3, 4, 5, 6, 7, 9, 10] } }))
 
-console.log('user no tag', userRepo.create({ username: 'My User2' }))
+console.log('user no tag', userRepo.create({ username: `user_notag_${randomString(10)}` }))
